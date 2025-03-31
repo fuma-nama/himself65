1fcfeb0d1cddbed1b4409ff7dae229b8cb19ae0f
Alex Yang <himself65@outlook.com>
2025-03-30 17:14:58 -0700

    fix: ssr issue

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -9,19 +9,12 @@ type RootLayoutProps = { children: ReactNode; path: string };
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html>
-      <head>
-        <title>Himself65</title>
-      </head>
-      <body>
-        <Layout>
-          <div className="flex min-h-screen flex-col items-start px-5 bg-white dark:bg-gray-900 text-xl font-light dark:text-gray-300">
-            {children}
-            <Banner />
-          </div>
-        </Layout>
-      </body>
-    </html>
+    <Layout>
+      <div className="flex min-h-screen flex-col items-start px-5 bg-white dark:bg-gray-900 text-xl font-light dark:text-gray-300">
+        {children}
+        <Banner />
+      </div>
+    </Layout>
   );
 }
 
