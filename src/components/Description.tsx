4d20ec08561cb1f738554d9df676e9fcbfbae454
Alex Yang <himself65@outlook.com>
2025-05-04 04:10:48 -0700

    feat: update description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,25 +1,188 @@
 export const Description = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold sm:text-5xl dark:text-gray-200">
-        I&apos;m Alex Yang
+      <h1 className="text-2xl font-bold sm:text-5xl dark:text-gray-200">
+        Alex Yang
       </h1>
       <div className="flex flex-col items-start w-full flex-1 text-start pt-5 sm:pt-10 px-0 sm:px-10 lg:px-40">
-        <p>
-          A student and a open-source developer, focusing on building
-          large-scale frontend applications.
+        <p className="text-sm sm:text-base dark:text-gray-200">
+          A open-source developer, npm library author and maintainer.
         </p>
-        <p>
-          I'm working for{" "}
-          <a
-            className="text-blue-500 hover:underline"
-            href="https://www.llamaindex.ai"
-            target="_blank"
-          >
-            LlamaIndex
-          </a>{" "}
-          as a software engineer.
+        <p className="text-sm sm:text-base dark:text-gray-200">
+          Solving problems using Nodejs and React.
         </p>
+        <h2 className="text-lg font-bold sm:text-xl dark:text-gray-200 pt-5 sm:pt-10">
+          work
+        </h2>
+        <ul className="list-disc list-inside text-sm sm:text-base dark:text-gray-200">
+          <li>
+            LlamaIndex, <b className="font-bold">software engineer</b> (nov 2023
+            - present){" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://llamaindex.ai/"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            toeverything, <b className="font-bold">frontend tech lead</b> (dec
+            2022 - oct 2023){" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://affine.pro/"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            Textea, <b className="font-bold">co-founder</b> (may 2022 - dec
+            2022){" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://www.textea.co/"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            Maskbook, <b className="font-bold">frontend internship</b> (may 2021
+            - aug 2021){" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="http://mask.io/"
+            >
+              ↗
+            </a>
+          </li>
+          <li>...</li>
+        </ul>
+        <h2 className="text-lg font-bold sm:text-xl dark:text-gray-200 pt-5 sm:pt-10">
+          authored libraries
+        </h2>
+        <ul className="list-disc list-inside text-sm sm:text-base dark:text-gray-200">
+          <li>
+            @llama-flow/core{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/run-llama/llama-flow"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            jotai-ai{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/jotaijs/jotai-ai"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            @toeverything/y-indexeddb{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://www.npmjs.com/package/@toeverything/y-indexeddb"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            @textea/json-viewer{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://www.npmjs.com/package/@textea/json-viewer"
+            >
+              ↗
+            </a>
+          </li>
+          <li>...</li>
+        </ul>
+        <h2 className="text-lg font-bold sm:text-xl dark:text-gray-200 pt-5 sm:pt-10">
+          honored to be a maintainer of
+        </h2>
+        <ul className="list-disc list-inside text-sm sm:text-base dark:text-gray-200">
+          <li>
+            athena AI{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/Athena-AI-Lab/athena-core"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            waku{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/dai-shi/waku"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            jotai{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/pmndrs/jotai"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            <del>AFFiNE</del>{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/toeverything/AFFiNE"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            <del>blocksuite</del>{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/toeverything/blocksuite"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            <del>Maskbook</del>{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/DimensionDev/Maskbook"
+            >
+              ↗
+            </a>
+          </li>
+          <li>
+            Node.js{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://github.com/nodejs/node"
+            >
+              ↗
+            </a>
+          </li>
+          <li>...</li>
+        </ul>
       </div>
     </>
   );
